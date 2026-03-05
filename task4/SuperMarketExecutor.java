c2a83cc98673b76fa7a8ba39b7851ad8ff7d5f67
geethajadar35 <geethajadar35@gmail.com>
2026-03-05 10:58:44 +0530

    task4

---
+++ task4/SuperMarketExecutor.java
@@ -0,0 +1,368 @@
+class SuperMarketExecutor
+{
+	public static void main(String [] args)
+	{
+	String biscuitBrands[] = SuperMarket.searchBrandByProduct("Biscuit");
+	for(String biscuitBrand : biscuitBrands){
+		System.out.println(biscuitBrand);
+	}
+	
+	String shampooBrands[] = SuperMarket.searchBrandByProduct("Shampoo");
+	for(String shampooBrand : shampooBrands){
+		System.out.println(shampooBrand);
+	}
+	
+	String soapBrands[] = SuperMarket.searchBrandByProduct("Soap");
+	for(String soapBrand : soapBrands){
+		System.out.println(soapBrand);
+	}
+	
+	String toothpasteBrands[] = SuperMarket.searchBrandByProduct("Toothpaste");
+	for(String toothpasteBrand : toothpasteBrands){
+		System.out.println(toothpasteBrand);
+	}
+	
+	String oilBrands[] = SuperMarket.searchBrandByProduct("CookingOil");
+	for(String oilBrand : oilBrands){
+		System.out.println(oilBrand);
+	}
+	
+	String milkBrands[] = SuperMarket.searchBrandByProduct("Milk");
+	for(String milkBrand : milkBrands){
+		System.out.println(milkBrand);
+	}
+	
+	String riceBrands[] = SuperMarket.searchBrandByProduct("Rice");
+	for(String riceBrand : riceBrands){
+		System.out.println(riceBrand);
+	}
+	
+	String chocolateBrands[] = SuperMarket.searchBrandByProduct("Chocolate");
+	for(String chocolateBrand : chocolateBrands){
+		System.out.println(chocolateBrand);
+	}
+	
+	String teaBrands[] = SuperMarket.searchBrandByProduct("Tea");
+	for(String teaBrand : teaBrands){
+		System.out.println(teaBrand);
+	}
+	
+	String coffeeBrands[] = SuperMarket.searchBrandByProduct("Coffee");
+	for(String coffeeBrand : coffeeBrands){
+		System.out.println(coffeeBrand);
+	}
+	
+	String refrigeratorBrands[] = SuperMarket.searchBrandByProduct("Refrigerator");
+	for(String refrigeratorBrand : refrigeratorBrands){
+		System.out.println(refrigeratorBrand);
+	}
+	
+	String acBrands[] = SuperMarket.searchBrandByProduct("AirConditioner");
+	for(String acBrand : acBrands){
+		System.out.println(acBrand);
+	}
+	
+	String laptopBrands[] = SuperMarket.searchBrandByProduct("Laptop");
+	for(String laptopBrand : laptopBrands){
+		System.out.println(laptopBrand);
+	}
+	
+	String tvBrands[] = SuperMarket.searchBrandByProduct("Television");
+	for(String tvBrand : tvBrands){
+		System.out.println(tvBrand);
+	}
+	
+	String shoeBrands[] = SuperMarket.searchBrandByProduct("Shoes");
+	for(String shoeBrand: shoeBrands){
+		System.out.println(shoeBrand);
+	}
+	
+	String attaBrands[] = SuperMarket.searchBrandByProduct("Atta");
+	for(String attaBrand : attaBrands){
+		System.out.println(attaBrand);
+	}
+	
+	String sugarBrands[] = SuperMarket.searchBrandByProduct("Sugar");
+	for(String sugarBrand : sugarBrands){
+		System.out.println(sugarBrand);
+	}
+	
+	String saltBrands[] = SuperMarket.searchBrandByProduct("Salt");
+	for(String saltBrand : saltBrands){
+		System.out.println(saltBrand);
+	}
+	
+	String turmericBrands[] = SuperMarket.searchBrandByProduct("Turmeric");
+	for(String turmericBrand : turmericBrands){
+		System.out.println(turmericBrand);
+	}
+	
+	String chilliBrands[] = SuperMarket.searchBrandByProduct("ChilliPowder");
+	for(String chilliBrand : chilliBrands){
+		System.out.println(chilliBrand);
+	}
+	
+	String gheeBrands[] = SuperMarket.searchBrandByProduct("Ghee");
+	for(String gheeBrand : gheeBrands){
+		System.out.println(gheeBrand);
+	}
+	
+	String moongDalBrands[] = SuperMarket.searchBrandByProduct("MoongDal");
+	for(String moongDalBrand : moongDalBrands){
+		System.out.println(moongDalBrand);
+	}
+	
+	String pickleBrands[] = SuperMarket.searchBrandByProduct("Pickle");
+	for(String pickleBrand : pickleBrands){
+		System.out.println(pickleBrand);
+	}
+	
+	String uradDalBrands[] = SuperMarket.searchBrandByProduct("UradDal");
+	for(String uradDalBrand : uradDalBrands){
+		System.out.println(uradDalBrand);
+	}
+	
+	String toorDalBrands[] = SuperMarket.searchBrandByProduct("ToorDal");
+	for(String toorDalBrand : toorDalBrands){
+		System.out.println(toorDalBrand);
+	}
+	
+	String MasoorDalBrands[] = SuperMarket.searchBrandByProduct("MasoorDal");
+	for(String MasoorDalBrand : MasoorDalBrands){
+		System.out.println(MasoorDalBrand);
+	}
+	
+	String juiceBrands[] = SuperMarket.searchBrandByProduct("Juice");
+	for(String juiceBrand : juiceBrands){
+		System.out.println(juiceBrand);
+	}
+	
+	String iceCreamBrands[] = SuperMarket.searchBrandByProduct("IceCream");
+	for(String iceCreamBrand : iceCreamBrands){
+		System.out.println(iceCreamBrand);
+	}
+	
+	String honeyBrands[] = SuperMarket.searchBrandByProduct("Honey");
+	for(String honeyBrand : honeyBrands){
+		System.out.println(honeyBrand);
+	}
+	
+	String cookingMasalaBrands[] = SuperMarket.searchBrandByProduct("CookingMasala");
+	for(String cookingMasalaBrand : cookingMasalaBrands){
+		System.out.println(cookingMasalaBrand);
+	}
+	
+	String snacksBrands[] = SuperMarket.searchBrandByProduct("Snacks");
+	for(String snacksBrand : snacksBrands){
+		System.out.println(snacksBrand);
+	}
+	
+	String softDrinksBrands[] = SuperMarket.searchBrandByProduct("SoftDrinks");
+	for(String softDrinksBrand : softDrinksBrands){
+		System.out.println(softDrinksBrand);
+	}
+	
+	String cookingOilPremiumBrands[] = SuperMarket.searchBrandByProduct("CookingOilPremium");
+	for(String cookingOilPremiumBrand : cookingOilPremiumBrands){
+		System.out.println(cookingOilPremiumBrand);
+	}
+	
+	String saucesBrands[] = SuperMarket.searchBrandByProduct("Sauces");
+	for(String saucesBrand : saucesBrands){
+		System.out.println(saucesBrand);
+	}
+	
+	String ketchupBrands[] = SuperMarket.searchBrandByProduct("Ketchup");
+	for(String ketchupBrand : ketchupBrands){
+		System.out.println(ketchupBrand);
+	}
+	
+	String namkeenBrands[] = SuperMarket.searchBrandByProduct("Namkeen");
+	for(String namkeenBrand : namkeenBrands){
+		System.out.println(namkeenBrand);
+	}
+	
+	String cleaningPowderBrands[] = SuperMarket.searchBrandByProduct("CleaningPowder");
+	for(String cleaningPowderBrand : cleaningPowderBrands){
+		System.out.println(cleaningPowderBrand);
+	}
+	
+	String personalCareBrands[] = SuperMarket.searchBrandByProduct("PersonalCare");
+	for(String personalCareBrand : personalCareBrands){
+		System.out.println(personalCareBrand);
+	}
+	
+	String herbalTeaBrands[] = SuperMarket.searchBrandByProduct("HerbalTeas");
+	for(String herbalTeaBrand : herbalTeaBrands){
+		System.out.println(herbalTeaBrand);
+	}
+	
+	String mobileBrands[] = SuperMarket.searchBrandByProduct("MobilePhones");
+	for(String mobileBrand : mobileBrands){
+		System.out.println(mobileBrand);
+	}
+	
+	String tabletBrands[] = SuperMarket.searchBrandByProduct("Tablets");
+	for(String tabletBrand : tabletBrands){
+		System.out.println(tabletBrand);
+	}
+	
+	String watchBrands[] = SuperMarket.searchBrandByProduct("Watches");
+	for(String watchBrand : watchBrands){
+		System.out.println(watchBrand);
+	}
+	
+	String drinkBrands[] = SuperMarket.searchBrandByProduct("SoftDrinks");
+	for(String drinkBrand : drinkBrands){
+		System.out.println(drinkBrand);
+	}
+	
+	String MoisturizerBrands[] = SuperMarket.searchBrandByProduct("Moisturizer");
+	for(String MoisturizerBrand : MoisturizerBrands){
+		System.out.println(MoisturizerBrand);
+	}
+	
+	String cleanerBrands[] = SuperMarket.searchBrandByProduct("Cleaner");
+	for(String cleanerBrand : cleanerBrands){
+		System.out.println(cleanerBrand);
+	}
+	
+	String detergentBrands[] = SuperMarket.searchBrandByProduct("Detergent");
+	for(String detergentBrand : detergentBrands){
+		System.out.println(detergentBrand);
+	}
+	
+	String roseWaterBrands[] = SuperMarket.searchBrandByProduct("RoseWater");
+	for(String roseWaterBrand : roseWaterBrands){
+		System.out.println(roseWaterBrand);
+	}
+	
+	String flourAlternativeBrands[] = SuperMarket.searchBrandByProduct("FlourAlternative");
+	for(String flourAlternativeBrand : flourAlternativeBrands){
+		System.out.println(flourAlternativeBrand);
+	}
+	
+	String sunCreamBrands[] = SuperMarket.searchBrandByProduct("SunCream");
+	for(String sunCreamBrand : sunCreamBrands){
+		System.out.println(sunCreamBrand);
+	}
+	
+	String deoBrands[] = SuperMarket.searchBrandByProduct("Deodorants");
+	for(String deoBrand : deoBrands){
+		System.out.println(deoBrand);
+	}
+	
+	String handwashBrands[] = SuperMarket.searchBrandByProduct("Handwash");
+	for(String handwashBrand : handwashBrands){
+		System.out.println(handwashBrand);
+	}
+	
+	String headphoneBrands[] = SuperMarket.searchBrandByProduct("Headphones");
+	for(String headphoneBrand : headphoneBrands){
+		System.out.println(headphoneBrand);
+	}
+	
+	String bagBrands[] = SuperMarket.searchBrandByProduct("Backpacks");
+	for(String bagBrand : bagBrands){
+		System.out.println(bagBrand);
+	}
+	
+	String cookerBrands[] = SuperMarket.searchBrandByProduct("PressureCooker");
+	for(String cookerBrand : cookerBrands){
+		System.out.println(cookerBrand);
+	}
+	
+	String bottleBrands[] = SuperMarket.searchBrandByProduct("WaterBottles");
+	for(String bottleBrand : bottleBrands){
+		System.out.println(bottleBrand);
+	}
+	
+	String brushBrands[] = SuperMarket.searchBrandByProduct("ToothBrush");
+	for(String brushBrand : brushBrands){
+		System.out.println(brushBrand);
+	}
+	
+	String fanBrands[] = SuperMarket.searchBrandByProduct("CeilingFan");
+	for(String fanBrand : fanBrands){
+		System.out.println(fanBrand);
+	}
+	
+	String stoveBrands[] = SuperMarket.searchBrandByProduct("GasStove");
+	for(String stoveBrand : stoveBrands){
+		System.out.println(stoveBrand);
+	}
+	
+	String mixerBrands[] = SuperMarket.searchBrandByProduct("MixerGrinder");
+	for(String mixerBrand : mixerBrands){
+		System.out.println(mixerBrand);
+	}
+	
+	String mattressBrands[] = SuperMarket.searchBrandByProduct("Mattress");
+	for(String mattressBrand : mattressBrands){
+		System.out.println(mattressBrand);
+	}
+	
+	String carCareBrands[] = SuperMarket.searchBrandByProduct("CarShampoo");
+	for(String carCareBrand : carCareBrands){
+		System.out.println(carCareBrand);
+	}
+	
+	String powerBankBrands[] = SuperMarket.searchBrandByProduct("PowerBank");
+	for(String powerBankBrand : powerBankBrands){
+		System.out.println(powerBankBrand);
+	}
+	
+	String speakerBrands[] = SuperMarket.searchBrandByProduct("BluetoothSpeaker");
+	for(String speakerBrand : speakerBrands){
+		System.out.println(speakerBrand);
+	}
+	
+	String trimmerBrands[] = SuperMarket.searchBrandByProduct("Trimmer");
+	for(String trimmerBrand : trimmerBrands){
+		System.out.println(trimmerBrand);
+	}
+	
+	String notebookBrands[] = SuperMarket.searchBrandByProduct("Notebook");
+	for(String notebookBrand : notebookBrands){
+		System.out.println(notebookBrand);
+	}
+	
+	String penBrands[] = SuperMarket.searchBrandByProduct("Pen");
+	for(String penBrand : penBrands){
+		System.out.println(penBrand);
+	}
+	
+	String pencilBrands[] = SuperMarket.searchBrandByProduct("Pencil");
+	for(String pencilBrand : pencilBrands){
+		System.out.println(pencilBrand);
+	}
+	
+	String lotionBrands[] = SuperMarket.searchBrandByProduct("BodyLotion");
+	for(String lotionBrand : lotionBrands){
+		System.out.println(lotionBrand);
+	}
+	
+	String shavingBrands[] = SuperMarket.searchBrandByProduct("ShavingCream");
+	for(String shavingBrand : shavingBrands){
+		System.out.println(shavingBrand);
+	}
+	
+	String lunchbagBrands[] = SuperMarket.searchBrandByProduct("LunchBag");
+	for(String lunchbagBrand : lunchbagBrands){
+		System.out.println(lunchbagBrand);
+	}
+	
+	String curtainBrands[] = SuperMarket.searchBrandByProduct("Curtains");
+	for(String curtainBrand : curtainBrands){
+		System.out.println(curtainBrand);
+	}
+	
+	String clockBrands[] = SuperMarket.searchBrandByProduct("WallClock");
+	for(String clockBrand : clockBrands){
+		System.out.println(clockBrand);
+	}
+	
+	
+	
+	}
+}
