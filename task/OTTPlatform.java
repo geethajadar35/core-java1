370f7b04877ab3ab1cf533669d8767a2a55b6d08
geethajadar35 <geethajadar35@gmail.com>
2026-03-05 10:15:03 +0530

    task

---
+++ task/OTTPlatform.java
@@ -0,0 +1,228 @@
+class Amozon Prime
+{
+public static void main(String[] args)
+{
+
+String ottName="Netflix";
+String movieslanguage[]={"Kannada","English","Hindi","Tamil","Telugu","Malayalam"};
+String kannadaMovies []={"KGF:Chapter 1","KGF:Chapter 2","Kantara","Salar","Vikrant Rona","Mufti","Tagaru","Ugram","UI","Dasara","Bhageera","Hi Nanna","Kurup",
+"Rathnan Prapancha","Thithi","U-Turn","Kahi","Urvi","Hombanna","Murphy","Aana","Beast","Katteri","Virupaksha","The Ghost","Dia","Charlie","Love Mocktail","Lucia","Ranna"};
+
+String englishMovies []={"Enola holmes","The king","My oxford year","The Guilty","The Wonder","Don't Look Up","The IrishMan","Bird Box","6 Underground",
+"The Thursday Murder Club","The gray man","Heart of Stone","The Dig","Outlaw king","Havoc","He's the best","Rebecca","Enola Holmes2","Murder Mystery","carry on","Joy",
+"The Electric state","Red Notice","The life list","The Unforgivable","Triple Frontier","Reptile","The Half of it","Extraction","Jay Kelly"};
+
+String hindiMovies []={"tehran","jaat","dhadak 2","sector 36","khufiya","bheed","deva","do patti","major","jaane jaan","pagglait","guilty","article 15",
+"roohi","raid 2","kathal","ittefaq","shaitaan","badla","chhaava","sikandar","dhamaka","jigra","ludo","maska","war","bulbbul","mardaani 2","animal","anek"};
+
+String tamilMovies []={"kaantha","aaryan","dude","dear","maareesan","visaranai","theera kaadhal","thug life","perusu","oho enthan baby","sinam",
+"sillu karuppatti","good bad ugly","retro","maharaj","vaathi","meiyazhagan","Enthiran","2.0","Vikram","Master","Jailer","Leo","Bigil","Kaithi","Asuran","Viswasam",
+"Petta","Mersal","Theri","nniyan","Ghajini","Sivaji","Kabali","Darbar","Ratsasan","Soorarai Pottru","96","Vada Chennai","Karnan","Ponniyin Selvan 1"};
+
+String teluguMovies []={"premante","the girlfriend","telusu kada","little hearts","champion","8 vasantalu","court","kingdom","thammudu","thandel","Jersey",
+"Rangasthalam","Dear Comrade","Maharshi","Bharat Ane Nenu","KGF Chapter 1","KGF Chapter 2","Chatrapathi","Athadu","Pokiri",
+"Temper","Julayi","Gabbar Singh","Dookudu","Khaidi No 150","Oopiri","Geetha Govindam","Hello","Acharya","Vikramarkudu"};
+
+String malayalamMovies[] = {"Drishyam","Premam","Lucifer","Pulimurugan","Bangalore Days","Kumbalangi Nights","Jana Gana Mana","Ayyappanum Koshiyum","Minnal Murali","2018",
+"Neru","Rorschach","Trance","Jallikattu","The Great Indian Kitchen","Anjaam Pathiraa","Nayattu","Charlie","Ustad Hotel","Take Off","Oppam","Ishq","Android Kunjappan",
+"Kurup","Bheeshma Parvam","CBI 5","Thallumaala","Malik","Kaathal","Home"};
+
+System.out.println("The Ott Platform Name is :" +ottName);
+System.out.println("List the Different Language Category of movies :");
+System.out.println("List of kannada platform :");
+
+System.out.println(kannadaMovies[0]+"\n"+
+				   kannadaMovies[1]+"\n"+
+				   kannadaMovies[2]+"\n"+
+				   kannadaMovies[3]+"\n"+
+				   kannadaMovies[4]+"\n"+
+				   kannadaMovies[5]+"\n"+
+				   kannadaMovies[6]+"\n"+
+				   kannadaMovies[7]+"\n"+
+				   kannadaMovies[8]+"\n"+
+				   kannadaMovies[9]+"\n"+
+				   kannadaMovies[10]+"\n"+
+				   kannadaMovies[11]+"\n"+
+				   kannadaMovies[12]+"\n"+
+				   kannadaMovies[13]+"\n"+
+				   kannadaMovies[14]+"\n"+
+				   kannadaMovies[15]+"\n"+
+				   kannadaMovies[16]+"\n"+
+				   kannadaMovies[17]+"\n"+
+				   kannadaMovies[18]+"\n"+
+				   kannadaMovies[19]+"\n"+
+				   kannadaMovies[20]+"\n"+
+				   kannadaMovies[21]+"\n"+
+				   kannadaMovies[22]+"\n"+
+				   kannadaMovies[23]+"\n"+
+				   kannadaMovies[24]+"\n"+
+				   kannadaMovies[25]+"\n"+
+				   kannadaMovies[26]+"\n"+
+				   kannadaMovies[27]+"\n"+
+				   kannadaMovies[28]+"\n"+
+				   kannadaMovies[29]+"\n");
+				   
+System.out.println("List of english films :");
+				   
+System.out.println(englishMovies[0]+"\n"+
+				   englishMovies[1]+"\n"+
+				   englishMovies[2]+"\n"+
+				   englishMovies[3]+"\n"+
+				   englishMovies[4]+"\n"+
+				   englishMovies[5]+"\n"+
+				   englishMovies[6]+"\n"+
+				   englishMovies[7]+"\n"+
+				   englishMovies[8]+"\n"+
+				   englishMovies[9]+"\n"+
+				   englishMovies[10]+"\n"+
+				   englishMovies[11]+"\n"+
+				   englishMovies[12]+"\n"+
+				   englishMovies[13]+"\n"+
+				   englishMovies[14]+"\n"+
+				   englishMovies[15]+"\n"+
+				   englishMovies[16]+"\n"+
+				   englishMovies[17]+"\n"+
+				   englishMovies[18]+"\n"+
+				   englishMovies[19]+"\n"+
+				   englishMovies[20]+"\n"+
+				   englishMovies[21]+"\n"+
+				   englishMovies[22]+"\n"+
+				   englishMovies[23]+"\n"+
+				   englishMovies[24]+"\n"+
+				   englishMovies[25]+"\n"+
+				   englishMovies[26]+"\n"+
+				   englishMovies[27]+"\n"+
+				   englishMovies[28]+"\n"+
+				   englishMovies[29]+"\n");
+
+System.out.println("List of hindi films :");
+System.out.println(hindiMovies[0]+"\n"+
+				   hindiMovies[1]+"\n"+
+				   hindiMovies[2]+"\n"+
+				   hindiMovies[3]+"\n"+
+				   hindiMovies[4]+"\n"+
+				   hindiMovies[5]+"\n"+
+				   hindiMovies[6]+"\n"+
+				   hindiMovies[7]+"\n"+
+				   hindiMovies[8]+"\n"+
+				   hindiMovies[9]+"\n"+
+				   hindiMovies[10]+"\n"+
+				   hindiMovies[11]+"\n"+
+				   hindiMovies[12]+"\n"+
+				   hindiMovies[13]+"\n"+
+				   hindiMovies[14]+"\n"+
+				   hindiMovies[15]+"\n"+
+				   hindiMovies[16]+"\n"+
+				   hindiMovies[17]+"\n"+
+				   hindiMovies[18]+"\n"+
+				   hindiMovies[19]+"\n"+
+				   hindiMovies[20]+"\n"+
+				   hindiMovies[21]+"\n"+
+				   hindiMovies[22]+"\n"+
+				   hindiMovies[23]+"\n"+
+				   hindiMovies[24]+"\n"+
+				   hindiMovies[25]+"\n"+
+				   hindiMovies[26]+"\n"+
+				   hindiMovies[27]+"\n"+
+				   hindiMovies[28]+"\n"+
+				   hindiMovies[29]+"\n");
+				   
+System.out.println("List of tamil films:");
+System.out.println(tamilMovies[0]+"\n"+
+                   tamilMovies[1]+"\n"+
+                   tamilMovies[2]+"\n"+
+                   tamilMovies[3]+"\n"+
+                   tamilMovies[4]+"\n"+
+                   tamilMovies[5]+"\n"+
+                   tamilMovies[6]+"\n"+
+                   tamilMovies[7]+"\n"+
+                   tamilMovies[8]+"\n"+
+                   tamilMovies[9]+"\n"+
+                   tamilMovies[10]+"\n"+
+                   tamilMovies[11]+"\n"+
+                   tamilMovies[12]+"\n"+
+                   tamilMovies[13]+"\n"+
+                   tamilMovies[14]+"\n"+
+                   tamilMovies[15]+"\n"+
+                   tamilMovies[16]+"\n"+
+                   tamilMovies[17]+"\n"+
+                   tamilMovies[18]+"\n"+
+                   tamilMovies[19]+"\n"+
+                   tamilMovies[20]+"\n"+
+                   tamilMovies[21]+"\n"+
+                   tamilMovies[22]+"\n"+
+                   tamilMovies[23]+"\n"+
+                   tamilMovies[24]+"\n"+
+                   tamilMovies[25]+"\n"+
+                   tamilMovies[26]+"\n"+
+                   tamilMovies[27]+"\n"+
+                   tamilMovies[28]+"\n"+
+                   tamilMovies[29]+"\n");
+
+System.out.println("List of telgu films :");
+System.out.println(teluguMovies[0]+"\n"+
+                   teluguMovies[1]+"\n"+
+                   teluguMovies[2]+"\n"+
+                   teluguMovies[3]+"\n"+
+                   teluguMovies[4]+"\n"+
+                   teluguMovies[5]+"\n"+
+                   teluguMovies[6]+"\n"+
+                   teluguMovies[7]+"\n"+
+                   teluguMovies[8]+"\n"+
+                   teluguMovies[9]+"\n"+
+                   teluguMovies[10]+"\n"+
+                   teluguMovies[11]+"\n"+
+                   teluguMovies[12]+"\n"+
+                   teluguMovies[13]+"\n"+
+                   teluguMovies[14]+"\n"+
+                   teluguMovies[15]+"\n"+
+                   teluguMovies[16]+"\n"+
+                   teluguMovies[17]+"\n"+
+                   teluguMovies[18]+"\n"+
+                   teluguMovies[19]+"\n"+
+                   teluguMovies[20]+"\n"+
+                   teluguMovies[21]+"\n"+
+                   teluguMovies[22]+"\n"+
+                   teluguMovies[23]+"\n"+
+                   teluguMovies[24]+"\n"+
+                   teluguMovies[25]+"\n"+
+                   teluguMovies[26]+"\n"+
+                   teluguMovies[27]+"\n"+
+                   teluguMovies[28]+"\n"+
+                   teluguMovies[29]+"\n");
+				   				   
+
+System.out.println("List of films :");
+System.out.println(malayalamMovies[0]+"\n"+
+                   malayalamMovies[1]+"\n"+
+                   malayalamMovies[2]+"\n"+
+                   malayalamMovies[3]+"\n"+
+                   malayalamMovies[4]+"\n"+
+                   malayalamMovies[5]+"\n"+
+                   malayalamMovies[6]+"\n"+
+                   malayalamMovies[7]+"\n"+
+                   malayalamMovies[8]+"\n"+
+                   malayalamMovies[9]+"\n"+
+                   malayalamMovies[10]+"\n"+
+                   malayalamMovies[11]+"\n"+
+                   malayalamMovies[12]+"\n"+
+                   malayalamMovies[13]+"\n"+
+                   malayalamMovies[14]+"\n"+
+                   malayalamMovies[15]+"\n"+
+                   malayalamMovies[16]+"\n"+
+                   malayalamMovies[17]+"\n"+
+                   malayalamMovies[18]+"\n"+
+                   malayalamMovies[19]+"\n"+
+                   malayalamMovies[20]+"\n"+
+                   malayalamMovies[21]+"\n"+
+                   malayalamMovies[22]+"\n"+
+                   malayalamMovies[23]+"\n"+
+                   malayalamMovies[24]+"\n"+
+                   malayalamMovies[25]+"\n"+
+                   malayalamMovies[26]+"\n"+
+                   malayalamMovies[27]+"\n"+
+                   malayalamMovies[28]+"\n"+
+                   malayalamMovies[29]+"\n");	
+
+}
+}
