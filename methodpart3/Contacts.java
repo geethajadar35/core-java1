a0b1a2e8e4ad944642ed2ec17adafbd760c7d103
geethajadar35 <geethajadar35@gmail.com>
2026-03-16 09:38:24 +0530

    methodpart3

---
+++ methodpart3/Contacts.java
@@ -0,0 +1,435 @@
+class Contacts
+{
+	static long searchContactsByName(String ContactsName)
+	{
+	
+	if(ContactsName.equalsIgnoreCase("Appa")){
+		return 9754628920L;
+	}
+	else if(ContactsName.equalsIgnoreCase("maa")){
+		return 9489768735L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandu")){
+		return 7909898058L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("vani")){
+		return 9745654788L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("vaishu")){
+		return 9734542532L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("nanda")){
+		return 9734542576L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ranju")){
+		return 9489767654L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("darshu")){
+		return 7909879087L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("brunda")){
+		return 9745676545L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("bhuvan")){
+		return 9734542598L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("deepu")){
+		return 9734542509L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("abhi")){
+		return 9489767612L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("rashi")){
+		return 7909879013L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chiki")){
+		return 9745676514L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chiku")){
+		return 9734542515L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ramesh")){
+		return 9734542516L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ragini")){
+		return 9489767617L; 
+	} 
+	else if(ContactsName.equalsIgnoreCase("keshuanna")){
+		return 7909879018L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ankal")){
+		return 9745676519L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ankitha")){
+		return 9734542523L; 
+	} 
+	else if(ContactsName.equalsIgnoreCase("anuantie")){
+		return 9734542565L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("anuhya")){
+		return 9489767987L; 
+	} 
+	else if(ContactsName.equalsIgnoreCase("anvitha")){
+		return 7909879098L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("archana")){
+		return 9745676763L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("arpitha")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("arunanna")){
+		return 9734542874L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("arunsir")){
+		return 9489767956L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ashamca")){
+		return 9489767998L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("asharaniakka")){
+		return 7909879097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ashitha")){
+		return 9745676796L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ashwinimakeover")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ashwiniscl")){
+		return 9734542882L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ashwinisis")){
+		return 7909877856L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ashwinitmp")){
+		return 9745677834L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("athmiya")){
+		return 9734547890L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("babyantie")){
+		return 9734547802L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("bindhu")){
+		return 9489760945L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("bindhutrailer")){
+		return 7909890962L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("brundha")){
+		return 9745650921L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("candrikackm")){
+		return 9734540987L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chaithra")){
+		return 9734540932L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chaithrabca")){
+		return 9489760945L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandanasenior")){
+		return 7909890962L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandanasgsis")){
+		return 9745650921L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandhana")){
+		return 9734540987L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandhanackm")){
+		return 9734540932L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandhanak")){
+		return 9489760908L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandhuk")){
+		return 7909870943L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandhup")){
+		return 9745670921L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandra")){
+		return 9734540927L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandrika")){
+		return 9734540976L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chandumca")){
+		return 9489767654L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("charu")){
+		return 7909879087L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("chathurthibca")){
+		return 9745676545L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("danalakshmi")){
+		return 9734542598L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("deekshithmca")){
+		return 9734542509L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("deepaksir")){
+		return 9489767654L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("deepika")){
+		return 7909879087L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("dekshitha")){
+		return 9745676545L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("dhanu")){
+		return 9734542598L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("dhanya")){
+		return 9734542509L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("dhodamma")){
+		return 9489767987L; 
+	} 
+	else if(ContactsName.equalsIgnoreCase("dhodappa")){
+		return 7909879098L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("dhumalli")){
+		return 9745676763L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("dineshmava")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("directormu")){
+		return 9734542874L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("divyabujji")){
+		return 9489767956L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("drrohi")){
+		return 9489767998L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("dvsxerox")){
+		return 7909879097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("faizu")){
+		return 9745676796L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("fansii")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("gagana")){
+		return 9489767987L; 
+	} 
+	else if(ContactsName.equalsIgnoreCase("ganashree")){
+		return 7909879098L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ganeshjewellery")){
+		return 9745676763L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("ganeshvinbnagar")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("geetha")){
+		return 9734542874L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("geetha1")){
+		return 7909879097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("girishbatru")){
+		return 9489767998L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("gourimca")){
+		return 7909879097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("gowthammca")){
+		return 9745676796L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("gvm")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("haleshsir")){
+		return 9731542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("haleshd")){
+		return 9732542882L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("harish")){
+		return 7903877856L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("harshini")){
+		return 9744677834L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("harshinibca")){
+		return 9735547890L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("harshitha")){
+		return 9736547802L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("heeba")){
+		return 9487760945L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("incharamca")){
+		return 7909890962L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("indhuja")){
+		return 9748650921L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("isiripg")){
+		return 9730540987L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("jaggaankal")){
+		return 9732540932L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("janyashree")){
+		return 7909821098L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("jnanavi")){
+		return 9745623763L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("joythika")){
+		return 9734524096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("joythi1")){
+		return 9734524096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kalilsir")){
+		return 9489726956L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kalyaniankal")){
+		return 9489727998L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kamalaauntie")){
+		return 7909828097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kamala1")){
+		return 7909828097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kanakashree")){
+		return 9734531096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kavanaakka")){
+		return 9489722987L; 
+	} 
+	else if(ContactsName.equalsIgnoreCase("kavanasharath")){
+		return 7909854098L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kavanaskg")){
+		return 9745653763L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kavya")){
+		return 9734572096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kavyaakkabnglr")){
+		return 9734571874L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kavyaanjali")){
+		return 9489773956L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kavyabarki")){
+		return 9489778998L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("keerthana")){
+		return 7909877097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("keerthanahyd")){
+		return 9745634796L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("keerthi")){
+		return 9734533096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("krish")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("kullamava")){
+		return 9489767987L; 
+	} 
+	else if(ContactsName.equalsIgnoreCase("lakshmitailor")){
+		return 7909879098L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("lathaantie")){
+		return 9745676763L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("lavanya")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("leelavegetable")){
+		return 9734542874L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("leelavathi")){
+		return 9489767956L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("likitha")){
+		return 9489767998L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("lohithanna")){
+		return 7909879097L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("lokeshmava")){
+		return 9745676796L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("malikadhodamma")){
+		return 9734542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("manjulaantie")){
+		return 9731542096L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("manjunathasir")){
+		return 9732542882L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("manu")){
+		return 7903877856L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("manubro")){
+		return 9744677834L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("medhu")){
+		return 9735547890L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("modelingsmg")){
+		return 9736547802L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("modell")){
+		return 9487760945L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("modell1")){
+		return 7909890962L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("modell2")){
+		return 9748650921L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("monisha")){
+		return 9730540987L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("naliniakka")){
+		return 7909890962L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("namanackm")){
+		return 9745650921L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("nandhinipg")){
+		return 9734540987L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("nanjundhaankal")){
+		return 9734540932L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("nayana")){
+		return 9489760908L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("nayanamca")){
+		return 7909870943L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("neha")){
+		return 9745670921L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("nehaakkimca")){
+		return 9734540927L; 
+	}
+	else if(ContactsName.equalsIgnoreCase("nehabca")){
+		return 9734540976L; 
+	}
+	else
+	{
+		System.out.println("Contacts Not Found");
+	}
+	return 0;
+}
+}
